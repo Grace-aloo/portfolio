326fb78602934e7d4579f803e677a41dc0067672
Grace Aloo <graceaaloo@gmail.com>
2025-05-25 18:15:08 +0300

    I finished the contact section

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const mobilemenu = document.querySelector('#mobile-menu');
+const navBar = document.querySelector('nav')
+const navLinks = document.querySelector('nav ul')
+
 
 function openMenu() {
     mobilemenu.classList.remove('translate-y-[-100%]');
@@ -9,3 +12,17 @@ function closeMenu() {
     mobilemenu.classList.remove('translate-y-0');
     mobilemenu.classList.add('translate-y-[-100%]');
 }
+window.addEventListener('scroll',()=>{
+    if(scrollY > 50){
+navBar.classList.add('bg-white/50', 'backdrop-blur-lg',
+    'shadow-sm'
+);
+navLinks.classList.remove('bg-white/50', 'shadow-sm')
+    }else{
+navBar.classList.remove('bg-white/50', 'backdrop-blur-lg',
+    'shadow-sm'
+);
+navLinks.classList.add('bg-white/50', 'shadow-sm')
+
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        }
+    },
+    colors:{
+        lighthover:'#fcf4ff',
+        darkhover:'#2a004a'
+    }
+}
